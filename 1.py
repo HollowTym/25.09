96d1759f4fa44d1cc20aa8197ccbda045905b4b4
Timyr486786866745 <90512751+Timyr486786866745@users.noreply.github.com>
2021-09-25 17:23:10 +0300

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,16 +1,89 @@
-class Football:
-    def name(self):
-        print('Дзюба', 'Маркевич', 'Рональдо', 'Месси', 'Саурес',
-              'Гризманн', 'Неймар', 'Бэйл', 'Брюйне', 'Агуэро', 'Нойер')
-
-    def pos(self):
-        print('1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11')
-
-
-def main():
-    foot = Football()
-    foot.name()
-    foot.pos()
 
 
 main()
+class Critter:
+    """фудболист"""
+    total = 0
+
+    def status():
+        print("Общее число фудболистов", Critter.total)
+
+    def __init__(self, name,):
+        self.__name = name
+        Critter.total += 1
+
+    def __str__(self):
+        ans = 'Объект класса Critter\n'
+        ans += 'имя: ' + self.name + '\n'
+        return ans
+
+    def name(self):
+        return self.__name
+
+
+def main1():
+    crit_name = input("Как вы назовете фудболиста?: ")
+    crit = Critter(crit_name)
+
+
+def main2():
+    crit_name = input("Как вы назовете фудболиста?: ")
+    crit = Critter(crit_name)
+
+
+def main3():
+    crit_name = input("Как вы назовете фудболиста?: ")
+    crit = Critter(crit_name)
+
+
+def main4():
+    crit_name = input("Как вы назовете фудболиста?: ")
+    crit = Critter(crit_name)
+
+
+def main5():
+    crit_name = input("Как вы назовете фудболиста?: ")
+    crit = Critter(crit_name)
+
+
+def main6():
+    crit_name = input("Как вы назовете фудболиста?: ")
+    crit = Critter(crit_name)
+
+
+def main7():
+    crit_name = input("Как вы назовете фудболиста?: ")
+    crit = Critter(crit_name)
+
+
+def main8():
+    crit_name = input("Как вы назовете фудболиста?: ")
+    crit = Critter(crit_name)
+
+
+def main9():
+    crit_name = input("Как вы назовете фудболиста?: ")
+    crit = Critter(crit_name)
+
+
+def main10():
+    crit_name = input("Как вы назовете фудболиста?: ")
+    crit = Critter(crit_name)
+
+
+def main11():
+    crit_name = input("Как вы назовете фудболиста?: ")
+    crit = Critter(crit_name)
+
+
+main1()
+main2()
+main3()
+main4()
+main5()
+main6()
+main7()
+main8()
+main9()
+main10()
+main11()
